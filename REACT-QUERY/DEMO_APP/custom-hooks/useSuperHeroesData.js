87ce6b9f2d4d811fe38d786fbfdf3bb5cf6cfb67
Vikash081995 <Vikash08.Kumar1995@gmail.com>
2022-11-26 20:19:25 +0530

    refactored

---
+++ REACT-QUERY/DEMO_APP/custom-hooks/useSuperHeroesData.js
@@ -0,0 +1,34 @@
+import { useQuery, useMutation, useQueryClient } from "react-query";
+import axios from "axios";
+
+const fetchSuperHeroes = () => {
+  return axios.get("http://localhost:4000/superheroes");
+};
+const addSuperHero = (hero) => {
+  return axios.post(`http://localhost:4000/superheroes`, hero);
+};
+export const useSuperHeroesData = (onSuccess, onError) => {
+  return useQuery("super-heroes", fetchSuperHeroes, {
+    onError,
+    onSuccess
+    // select: (data) => {
+    //   const superHeroNames = data.data.map((hero) => hero.name);
+    //   return superHeroNames;
+    // }
+  });
+};
+
+export const useAddSuperHeroData = () => {
+  const queryClient = useQueryClient();
+  return useMutation(addSuperHero, {
+    onSuccess: (data) => {
+      // queryClient.invalidateQueries('super-heroes')
+      queryClient.setQueryData("super-heroes", (oldQueryData) => {
+        return {
+          ...oldQueryData,
+          data: [...oldQueryData.data, data.data]
+        };
+      });
+    }
+  });
+};
