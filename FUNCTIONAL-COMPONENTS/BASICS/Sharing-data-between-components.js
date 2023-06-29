8affea29845af834bb4c00ae2816598596e6852a
Vikash081995 <Vikash08.Kumar1995@gmail.com>
2023-06-29 16:25:58 +0530

    React.dev oficial examples added

---
+++ FUNCTIONAL-COMPONENTS/BASICS/Sharing-data-between-components.js
@@ -0,0 +1,18 @@
+export default function MyApp() {
+  const [count, setCount] = useState(0);
+
+  const handleClick = () => {
+    setCount(count + 1);
+  };
+  return (
+    <div>
+      <h1>Counters that update together</h1>
+      <MyButton count={count} onClick={handleClick} />
+      <MyButton count={count} onClick={handleClick} />
+    </div>
+  );
+}
+
+function MyButton({ count, onClick }) {
+  return <button onClick={onClick}>Clicked {count} times</button>;
+}
